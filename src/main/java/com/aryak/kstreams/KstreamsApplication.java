39de6cc0aaba2c47f0bf134cfed5f56e8cdf068a
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2026-06-29 20:08:03 +0530

    Initial project structure

---
+++ src/main/java/com/aryak/kstreams/KstreamsApplication.java
@@ -0,0 +1,13 @@
+package com.aryak.kstreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KstreamsApplication.class, args);
+    }
+
+}
